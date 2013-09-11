4408157121a0a9c04436c1a4119c1ac4b9ad755d
Yotam Sela <yotam.sela@mail.huji.ac.il>
2013-09-11 18:22:17 +0300

    added application activity

--- client/src/com/example/gesturemouseclient/activities/CreateActionActivity.java
+++ client/src/com/example/gesturemouseclient/activities/CreateActionActivity.java
@@ -1,18 +1,15 @@
 package com.example.gesturemouseclient.activities;
 
-import com.example.gesturemouseclient.R;
-import com.example.gesturemouseclient.R.layout;
-import com.example.gesturemouseclient.R.menu;
-import com.example.gesturemouseclient.infra.Params;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.gesturemouseclient.R;
+import com.example.gesturemouseclient.infra.Params;
 
 public class CreateActionActivity extends Activity {
 
---
+++ client/src/com/example/gesturemouseclient/activities/CreateNewApplicationActivity.java
@@ -0,0 +1,69 @@
+package com.example.gesturemouseclient.activities;
+
+import com.example.gesturemouseclient.R;
+import com.example.gesturemouseclient.R.layout;
+import com.example.gesturemouseclient.R.menu;
+import com.example.gesturemouseclient.dal.ApplicationDAL;
+import com.example.gesturemouseclient.infra.Params;
+import com.example.gesturemouseclient.infra.RemoteDeviceInfo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateNewApplicationActivity extends Activity {
+
+	private Button createApplicationBtn;
+	private EditText applicationEditTxt;
+	private String processName;
+	private String windowTitle;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_new_application);
+		
+		createApplicationBtn = (Button) findViewById(R.id.createNewApplicationBtn);
+		applicationEditTxt = (EditText) findViewById(R.id.createApplicationTxt);
+		
+		Intent intent = getIntent();
+		processName = ((String) intent.getExtras().get("process_name")); 
+		windowTitle = ((String) intent.getExtras().get("window_title"));
+		
+		applicationEditTxt.setText(windowTitle);
+		
+		createApplicationBtn.setOnClickListener(new OnClickListener() {
+		
+			
+			@Override
+			public void onClick(View v) {
+				String newApplicationName = applicationEditTxt.getText().toString();
+				
+				
+				
+				if (windowTitle.startsWith(newApplicationName) || windowTitle.endsWith(newApplicationName)) {
+					if(newApplicationName.length() > 15){
+						ApplicationDAL applicationDAL = new ApplicationDAL(newApplicationName, processName, newApplicationName);
+						applicationDAL.save(getApplicationContext());
+						goBackToMainActivity(applicationDAL.getId());
+					}
+					
+				}
+				
+				
+			}
+		});
+	}
+
+	protected void goBackToMainActivity(Integer applicationId) {
+		Intent intent = new Intent(this, CreateGestureActivity.class);
+		intent.putExtra("app_id", applicationId);
+		finishActivity(Params.PICK_APPLICATION_REQUEST);
+	}
+
+}
